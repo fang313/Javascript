7eb7b34a15cd8a78c80231a8602b118d5d3666fa
vadimksendzov26 <fang.russian@gmail.com>
2022-05-05 00:02:41 +0300

    new solution

--- Homework 4 ex 2.js
+++ Homework 4 ex 2.js
@@ -1,32 +1,13 @@
 const jsonData= require('./task2 jsondata.json'); 
 
-let uniquedata = [];
-
-jsonData.forEach(function(jsonData ){
-    if (uniquedata.indexOf(jsonData) === -1 ){
-        uniquedata.push(jsonData)
+const result = []
+const hash = {}
+  
+  for (const item of jsonData) {
+    if (!hash[item.username]) {
+      hash[item.username] = true
+      result.push(item)
     }
-}
-)
-
-console.log(uniquedata)
-// for (let j=0;j<jsonData.length;j++){
-//     for (let i=0;i<jsonData.length;i++){
-//         if(i==j){
-//             continue;
-//         }
-//         else if(jsonData[i].name==jsonData[j].name)
-//         {
-//             // console.log(i + ' '+ j)
-//             jsonData.splice(j,1);
-//         }
-//     }
-// }
-// console.log(jsonData)
-
-
-
-
-// // usage example:
-// var a = ['a', 1, 'a', 2, '1'];
-// var unique = a.filter( onlyUnique ); // returns ['a', 1, 2, '1']
+  }
+  
+  console.log(result)
